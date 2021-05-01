f2da0c012bd40b8eaa43ad342fc9427dc4eb9129
JayJayOo <fatma.b.demirel@yandex.com>
2021-05-01 20:27:50 +0300

    Third_Commit_01_05'213

--- java_Camp_D1_H1&2&3_/src/java_Camp_D1_H3.java
+++ java_Camp_D1_H1&2&3_/src/java_Camp_D1_H3.java
@@ -1,4 +1,30 @@
 
 public class java_Camp_D1_H3 {
+	/*
+	 * Byte, short, int ve long tam sayý, float ve double ondalýk sayý tutmaktadýr.
+	 * TC Kimlik numarasý long olarak tutulur
+	 
+	 * Çok fazla if = spaghetti
+	 * Else = if þart bloðu dýþýndaki durum için kullanýlýr
+	 * Else if = Diðer bütün þartlar için, uygulamada açýk olmamasý adýna kullanýlýr 
+	  (Defensive programming yani savunmacý programlama)
+	 * Eþittir ifadesi deðer atamak için kullanýlýr
+	 * Deðere eþit mi þeklinde sorabilecek ifadeyi çift eþittir ifadesi saðlamaktadýr
+	 * Metamatiksel iþaretler operatör olarak adlandýrýlýr Küçüktür, büyüktür ve eþittir operatörü 
+	 
+	 * Deðiþken tanýmlanarak alt satýrda deðer atamasý da yapýlabilir
+	  Veya direk deðiþken oluþturulan yerde deðer atamasý da yapýlabilir
+	 * "String = "+deðer adý "En büyük = "+enBuyuk
+	 
+	 * If'ten baþka conditional þartlandýrma bloklarýndan biri de Switch'tir
+	   If bloðuna göre çok daha az kullanýlýr Suistimal etme ihtimali vardýr
+	 * break; programý bitirmek için kullanýlýr 
+	 * Noktalý virgül ise anlamýna gelir
+	 * default ise switch'in barýndýrdýðý bir case dýþýnda veri girilmesi durumunda devreye girer
+	 * Ctrl+Shift+Alt+L Ýdeyi bitirip Run demeden önce satýr veya girinti hatasý için düzenleme ve check adýna kullanýlýr
+	   Eclipse eklentileri de kullanýlabilir
+	  
+	 * 
+	 */
 
 }
