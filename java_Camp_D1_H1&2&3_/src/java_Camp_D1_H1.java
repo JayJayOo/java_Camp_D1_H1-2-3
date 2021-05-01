55a4f29554f64313e6d4bc519b85606fb2555212
JayJayOo <fatma.b.demirel@yandex.com>
2021-05-01 18:50:34 +0300

    First_Commit_01_05'21

---
+++ java_Camp_D1_H1&2&3_/src/java_Camp_D1_H1.java
@@ -0,0 +1,47 @@
+
+public class java_Camp_D1_H1 {
+
+/*
+
+	Primitive Data Types:
+	
+	boolean: Holds True and False values 
+	byte: Stores integers of size 8 bit
+	char: Stores a character
+	double: Double precision floating point
+	float: Single precision floating point
+	int: Stores a integer
+	long: Stores long integer
+	short: Stores a short integer
+
+	Non-Primitive Data Types:
+	
+	Class: a user-defined blueprint or prototype from which objects are created
+	Interface:  Like a class, an interface can have methods and variables, 
+	but the methods declared in an interface are by default abstract 
+	(only method signature, nobody)
+	String: An array of characters
+	Array: A group of like-typed variables that are referred to by a common name
+	
+
+	Non-primitive data types are called reference types because they refer to objects.
+	The main difference between primitive and non-primitive data types are:
+	1. Primitive types are predefined (already defined) in Java. 
+	Non-primitive types are created by the programmer and is not defined by Java 
+	2. Non-primitive types can be used to call methods to perform certain operation, 
+	while primitive types cannot
+	3. A primitive type has always value, while non-primitive types can be “NULL”
+	4. A primitive type start with a lowercase letter, 
+	while non-primitive types starts with an uppercase letterThe size of primitive type 
+	5. depends on the data type, while non-primitive types have all the same size.
+	
+	Strings: Store a sequence of characters (text)
+	*String values must be surrounded by double quotes (“”)
+
+	PS: The String type is so much used and integrated in Java, that some call it "the special ninth type".
+	A String in Java is actually a non-primitive data type, because it refers to an object. 
+	The String object has methods that are used to perform certain operations on strings. 
+	
+*/
+
+}
---
+++ java_Camp_D1_H1&2&3_/src/java_Camp_D1_H3.java
@@ -0,0 +1,4 @@
+
+public class java_Camp_D1_H3 {
+
+}
